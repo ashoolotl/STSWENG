b2a903a5b70f8708da1c2fb4ca396ce048ed69fb
xian <xianirabernardo@gmail.com>
2024-07-26 00:44:33 +0800

    fixed cart quantity limits

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,4 +1,5 @@
 const Cart = require("../models/cartModel");
+const Product = require("../models/productModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
@@ -14,14 +15,35 @@ exports.getItemsInCart = catchAsync(async (req, res, next) => {
 });
 
 exports.addItemsInCart = catchAsync(async (req, res, next) => {
-  console.log("req.body:", req.body);
-  const cart = await Cart.create(req.body);
-  res.status(200).json({
-    status: "success",
-    data: {
-      cart,
-    },
-  });
+  const itemInCart = await Cart.findOne({ product: req.body.product });
+  const product = await Product.findOne({ name: req.body.product });
+
+  if (itemInCart && itemInCart.plateNumber === "None") {
+    if (Number(itemInCart.quantity) + Number(req.body.quantity) <= Number(product.quantity)) {
+      itemInCart.quantity += Number(req.body.quantity);
+      itemInCart.price += Number(req.body.price);
+      await itemInCart.save();
+      return res.status(200).json({
+        status: "success",
+        data: {
+          cart: itemInCart,
+        },
+      });
+    } else {
+      return res.status(400).json({
+        status: "error",
+        message: "Not enough stock for this product",
+      });
+    }
+  } else {
+    const cart = await Cart.create(req.body);
+    res.status(200).json({
+      status: "success",
+      data: {
+        cart,
+      },
+    });
+  }
 });
 
 exports.removeItemInCart = catchAsync(async (req, res, next) => {
