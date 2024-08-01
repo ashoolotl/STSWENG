b523cdd692545da56de66536beee934a3e68520a
Kyle-Trinos <marvin_trinos@dlsu.edu.ph>
2024-08-02 06:42:41 +0800

    error handling in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,78 +16,121 @@ exports.getMe = (req, res, next) => {
     req.params.id = req.user.id;
     next();
 };
-exports.getUser = catchAsync(async (req, res, next) => {
-    const user = await User.findById(req.params.id);
-    console.log('Handler getUser executed. req.params.id:', req.params.id);
-    if (!user) {
-        return next(new AppError('No user found with that ID', 404));
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user,
-        },
-    });
-});
-exports.getUserById = catchAsync(async (req, res, next) => {
-    const user = await User.findById(req.params.userId);
-    if (!user) {
-        return next(new AppError('No user found with that ID', 404));
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user,
-        },
-    });
-});
-exports.getAllUser = catchAsync(async (req, res, next) => {
-    const users = await User.find();
 
-    res.status(200).json({
-        status: 'success',
-        results: users.length,
-        data: {
-            users,
-        },
-    });
+exports.getUser = catchAsync(async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        console.log('Handler getUser executed. req.params.id:', req.params.id);
+        if (!user) {
+            return next(new AppError('No user found with that ID', 404));
+        }
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'An error occurred.',
+        });
+    }
+});
+
+exports.getUserById = catchAsync(async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        if (!user) {
+            return next(new AppError('No user found with that ID', 404));
+        }
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'An error occurred.',
+        });
+    }
+});
+
+exports.getAllUser = catchAsync(async (req, res, next) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json({
+            status: 'success',
+            results: users.length,
+            data: {
+                users,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'An error occurred.',
+        });
+    }
 });
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-    // create error if user tried to change password
+    try {
+        // create error if user tried to change password
 
-    if (req.body.password || req.body.passwordConfirm) {
-        return next(
-            new AppError(
-                'This route is not for password updates. Please use /updateMyPassword',
-                400
-            )
-        );
-    }
-    // filter out field names that are not allowed to be updated
-    const filteredBody = filterObj(req.body, 'name', 'email');
-    const updatedUser = await User.findByIdAndUpdate(
-        req.user.id,
-        filteredBody,
-        {
-            new: true,
-            runValidators: true,
+        if (req.body.password || req.body.passwordConfirm) {
+            return next(
+                new AppError(
+                    'This route is not for password updates. Please use /updateMyPassword',
+                    400
+                )
+            );
         }
-    );
-    // filter the update me to fields only allowed to update
+        // filter out field names that are not allowed to be updated
+        const filteredBody = filterObj(req.body, 'name', 'email');
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user.id,
+            filteredBody,
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+        // filter the update me to fields only allowed to update
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user: updatedUser,
-        },
-    });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user: updatedUser,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'An error occurred.',
+        });
+    }
 });
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
-    await User.findByIdAndUpdate(req.user.id, { active: false });
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    });
+    try {
+        await User.findByIdAndUpdate(req.user.id, { active: false });
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'An error occurred.',
+        });
+    }
 });
