eb4434fc7ee268d3f6ea4160329a4d1d7bda1328
xian <xianirabernardo@gmail.com>
2024-08-02 01:59:52 +0800

    added error catching

--- controllers/availedServiceController.js
+++ controllers/availedServiceController.js
@@ -1,34 +1,55 @@
-const ServiceAvailed = require('../models/serviceAvailedModel');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const ServiceAvailed = require("../models/serviceAvailedModel");
+const AppError = require("../utils/appError");
 
-exports.getAllServiceAvailed = catchAsync(async (req, res, next) => {
+exports.getAllServiceAvailed = async (req, res, next) => {
+  try {
     const serviceAvailed = await ServiceAvailed.find();
 
     res.status(200).json({
-        status: 'success',
-        data: {
-            serviceAvailed,
-        },
+      status: "success",
+      data: {
+        serviceAvailed,
+      },
     });
-});
-exports.getAvailedServiceById = catchAsync(async (req, res, next) => {
+  } catch (err) {
+    next(new AppError("Failed to retrieve services availed", 500));
+  }
+};
+
+exports.getAvailedServiceById = async (req, res, next) => {
+  try {
     const serviceAvailed = await ServiceAvailed.findById(req.params.userId);
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            serviceAvailed,
-        },
-    });
-});
-exports.deleteAvailedService = catchAsync(async (req, res, next) => {
-    const deletedDoc = await ServiceAvailed.findOneAndDelete({
-        bookingId: req.params.userId,
-    });
+    if (!serviceAvailed) {
+      return next(new AppError("No service availed found with that ID", 404));
+    }
 
     res.status(200).json({
-        status: 'success',
-        data: null,
+      status: "success",
+      data: {
+        serviceAvailed,
+      },
     });
-});
+  } catch (err) {
+    next(new AppError("Failed to retrieve the service availed", 500));
+  }
+};
+
+exports.deleteAvailedService = async (req, res, next) => {
+  try {
+    const deletedDoc = await ServiceAvailed.findOneAndDelete({
+      bookingId: req.params.userId,
+    });
+
+    if (!deletedDoc) {
+      return next(new AppError("No service availed found with that booking ID", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    next(new AppError("Failed to delete the service availed", 500));
+  }
+};
